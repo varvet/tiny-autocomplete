234ebf562c0e77d6a73020178b65f490188d1d22
Johan Halse <johan.halse@varvet.com>
2013-12-21 13:27:22 +0100

    Initial pages commit

---
+++ README.md
@@ -0,0 +1,188 @@
+# Tiny Autocomplete
+A small and snappy (2k minified+gzipped!) JavaScript autocomplete plugin for Zepto and jQuery.
+
+## Why?
+Because it seems the only great autocomplete plugin out there is the jQuery UI one. It's a lovely plugin, both good-looking and robust, but it also requires you to pull in >100k of JavaScript and some 20k of CSS which makes it a non-starter for most projects, unless you're already using the jQuery UI components. And it doesn't support mobile or tablets! So the aim of Tiny Autocomplete is to make a plugin that's
+* Small footprint
+* Fast
+* Compatible with both Zepto and jQuery
+* Working on tablets and mobile devices
+
+Tiny Autocomplete also tries to be extendable. There's no abuse of closures or effort to make things private within the plugin, so if you want to monkey patch a built-in function, change a setting at runtime, or build a plugin for it - go right ahead!
+
+## Data and templating
+Tiny Autocomplete is a little picky with its JSON formatting. You can either just supply it with an array of objects (and those can contain arbitrary data), or you can feed it an array of groups. The grouped array items need a property called "data" that contains the array of objects instead. It's probably easiest to look at the local data examples if you need help. It's pretty straightforward. If you don't have control over your data source, you can overwrite the `onReceiveData` callback with something hand-rolled instead.
+
+Tiny Autocomplete also doesn't really assume it knows what your markup wants to look like. There's a built-in default but there's also a micro-templating engine in there which sounds kind of ominous but is actually really nifty. Since you can supply Tiny Autocomplete with your own little mustache-like template string, there's no magic "name" or "id" attribute or whatever. You can feed it a thumbnail, an icon, 5 levels of `<div>` wrapping - the sky's the limit.
+
+Note: while you can use Tiny Autocomplete with a local data source, the pattern matching implementation is quite naïve. Intelligently searching through a result set and returning it is what servers do really well, so maybe that's what you want to do?
+
+## Demos
+[Flat search with local data]
+
+[Grouped search with local data]
+
+[Flat search with remote data]
+
+[Grouped search with remote data]
+
+## Installation
+Just include tiny-autocomplete-0.x.js (development) or tiny-autocomplete-0.x-min.js (production) after Zepto or jQuery. Include tiny-autocomplete.css and you're good to go!
+
+## Usage
+### Initialize an autocomplete field
+```javascript
+$('.autocomplete').tinyAutocomplete({ /* settings */ });
+```
+
+
+### Settings
+#### url:
+```javascript
+$('.autocomplete').tinyAutocomplete({ url:'http://www.example.com/search' });
+```
+The url to run search queries against.
+
+
+#### data:
+```javascript
+$('.autocomplete').tinyAutocomplete({ data:[...] });
+```
+The formatted JSON data, either a flat array or a grouped one. Tiny Autocomplete will search *all* the fields in your data and return the items where it found a match.
+
+
+#### method:
+```javascript
+$('.autocomplete').tinyAutocomplete({ method:'post' });
+```
+Method to use - defaults to `get` but you can change it to `post`.
+
+
+#### onSelect:
+```javascript
+$('.autocomplete').tinyAutocomplete({ onSelect:function(el, val){...} });
+```
+This is the method that fires when user has selected an option in the autocomplete list, and it's scoped to the field the user is typing in. You get to decide what to do with the information - possibly load data via AJAX or redirect the user to the selected object's page or something even more clever. If user has just pressed enter without selecting an object, or has selected the lastItemTemplate object, the `val` argument will be blank and you should act on $(this).val() instead. Maybe go to the full search page?
+
+
+#### minChars:
+```javascript
+$('.autocomplete').tinyAutocomplete({ minChars:3 });
+```
+Minimum number of characters user has to type before autocomplete kicks in. Defaults to `2`.
+
+
+#### scrollOnFocus:
+```javascript
+$('.autocomplete').tinyAutocomplete({ scrollOnFocus:true });
+```
+For vertically-challenged touch devices that pop up an on-screen keyboard, it's a good idea to scroll the search field to the top. Tiny Autocomplete tries to guess if it's needed, but you might have a more intelligent heuristic. Then use that to set scrollOnFocus.
+
+
+#### keyboardDelay:
+```javascript
+$('.autocomplete').tinyAutocomplete({ keyboardDelay:400 });
+```
+The delay between user releasing the key and search being performed. Set this high to avoid firing lots of requests while the user is typing, or low for more responsiveness. Defaults to `200` and can be set to `null` to bypass it altogether.
+
+
+#### timeLimit:
+```javascript
+$('.autocomplete').tinyAutocomplete({ timeLimit:1000 });
+```
+Maximum amount of searches allowed in a given time frame. The last search is queued up and will fire as soon as it's allowed to do so.
+
+
+#### queryProperty:
+```javascript
+$('.autocomplete').tinyAutocomplete({ queryProperty:'term' });
+```
+The name of the request property to send in GET or POST request. Defaults to `q`.
+
+
+#### markAsBold:
+```javascript
+$('.autocomplete').tinyAutocomplete({ markAsBold:false });
+```
+Compare the result set with the typed string and wrap any matches in a `<strong />` tag. Processes all the properties in the matched JSON object. Defaults to `true`.
+
+
+#### grouped:
+```javascript
+$('.autocomplete').tinyAutocomplete({ grouped:true });
+```
+Whether or not Tiny Autocomplete should render the JSON as a grouped list. If set to false, it will expect a simple array of objects, if set to true it will expect the first-level objects to have a "data" property containing an array instead:
+```javascript
+[
+  {
+    "title": "Larks",
+    "coolness": "awesome",
+    "data": [
+      {"title":"Clapper Lark", "sound":"warbling" ...},
+      {"title":"Spike-heeled Lark", "weight":5 ...}
+  }
+]
+```
+
+
+#### itemTemplate:
+```javascript
+$('.autocomplete').tinyAutocomplete({ itemTemplate:'<li class="autocomplete-item">{{id}}: {{title}}</li>' });
+```
+Template for an item, using a pseudo-mustache engine. Objects will be passed to the template and it will perform string interpolation, replacing the {{mustachioed}} values with those from the object. For advanced usage, this can be overridden from the JSON-data on a per-group basis, like this:
+```javascript
+[
+  {
+    "title": "Larks",
+    "template": "<li class='autocomplete-item LARK-ITEM'>Lark: {{title}}</li>",
+    "data": [
+      {"title":"Clapper Lark"},
+      {"title":"Spike-heeled Lark"}
+  }
+]
+```
+Default template for an item is `<li class="autocomplete-item">{{title}}</li>`.
+
+
+#### groupTemplate:
+```javascript
+$('.autocomplete').tinyAutocomplete({ groupTemplate:'<li class="autocomplete-group"><h2>{{title}}</h2><ul class="autocomplete-items" /></li>' });
+```
+Change template for an item group, when using grouped view. Default is `<li class="autocomplete-group"><span class="autocomplete-group-header">{{title}}</span><ul class="autocomplete-items" /></li>`. Make sure your `groupContentName` matches the ul class!
+
+
+#### groupContentName:
+```javascript
+$('.autocomplete').tinyAutocomplete({ groupContentName: '.my-items'});
+```
+If you have changed the groupTemplate, you might have to tell Tiny Autocomplete where to put the group's items. Supply it with a class name and it will append the result items there.
+
+
+#### lastItemTemplate:
+```javascript
+$('.autocomplete').tinyAutocomplete({ lastItemTemplate: '<li class="autocomplete-item autocomplete-item-last">Show all results for "{{title}}"</li>' });
+```
+Set this template if you want to display a last item without any data bound to it. The field's current value will be supplied as {{title}}. This is useful for giving users an "all results for X" option at the bottom of the list.
+
+
+### Global defaults
+If you want to, you can set global options for all your autocompletes by setting them on the $.tinyAutocomplete.defaults object, like so:
+```javascript
+$.tinyAutocomplete.defaults.method = 'post';
+```
+
+### Change settings at runtime
+Easiest way is to just call
+```javascript
+$('.autocomplete').tinyAutocomplete({ settings... });
+```
+TinyAutocomplete won't instantiate again, it will just change the settings on the object that's already there. If you want to, you can access the settings object on the element itself - it's in el.tinyAutocomplete.settings.
+
+## License
+[MIT]
+
+[Flat search with local data]: http://tiny-autocomplete.hal.se/local.html
+[Grouped search with local data]: http://tiny-autocomplete.hal.se/grouped.html
+[Flat search with remote data]: http://tiny-autocomplete.hal.se/grouped.html
+[Grouped search with remote data]: http://tiny-autocomplete.hal.se
+[MIT]: http://johanhalse.mit-license.org
---
+++ src/tiny-autocomplete-0.3.js
@@ -0,0 +1,574 @@
+/**
+ * Tiny Autocomplete
+ * Small and fast autocomplete plugin for Zepto and jQuery.
+ * Written by Johan Halse, https://twitter.com/hejsna, johan@varvet.se
+ * License: http://johanhalse.mit-license.org
+ * @version 0.3
+ * @return {object}         Self
+ */
+(function(window, $) {
+  var TinyAutocomplete = function(el, options){
+    var that = this; // This is just to minify better
+    this.field = $(el);
+    that.el = null;
+    that.json = null;
+    that.items = [];
+    that.selectedItem = null;
+    that.list = $('<ul class="autocomplete-list" />');
+    that.lastSearch = null;
+    that.searchTimeout = null;
+    that.searchTimeLimit = null;
+    that.options = options;
+  };
+
+  TinyAutocomplete.prototype = {
+    defaults: {
+      minChars: 2,
+      markAsBold: true,
+      grouped: false,
+      queryProperty: 'q',
+      method: 'get',
+      scrollOnFocus: 'auto',
+      maxItems: 100,
+      timeLimit: null,
+      keyboardDelay: 200,
+      lastItemTemplate: null,
+      groupContentName: '.autocomplete-items',
+      groupTemplate: '<li class="autocomplete-group"><span class="autocomplete-group-header">{{title}}</span><ul class="autocomplete-items" /></li>',
+      itemTemplate: '<li class="autocomplete-item">{{title}}</li>'
+    },
+
+    /**
+     * Constructor
+     * @return {null}
+     */
+    init: function() {
+      this.settings = $.extend({}, this.defaults, this.options);
+      this.setupSettings();
+      this.setupMarkup();
+      this.setupEvents();
+
+      return this;
+    },
+
+    /**
+     * Micro-templating utility
+     * @param  {string} template Template string with mustache-style curly braces
+     * @param  {object} vars     Contains values to interpolate with
+     * @return {string}          Interpolated string
+     */
+    template: function(template, vars) {
+      return template.replace(/{{\s*[\w]+\s*}}/g, function(v) {
+        return vars[v.substr(2,v.length-4)];
+      });
+    },
+
+    /**
+     * Tweak settings a bit, since we don't have any private functions
+     * defined at constructor time.
+     * @return {null}
+     */
+    setupSettings: function() {
+      if(this.settings.scrollOnFocus == 'auto') {
+        this.settings.scrollOnFocus = this.isTouchDevice();
+      }
+
+      // We might be on a mobile device and have little in the way of
+      // vertical real estate to work with. Cap it! This check needs a
+      // bit more intelligence to it.
+      if($(window).height() < 500) {
+        this.settings.maxItems = Math.min( this.settings.maxItems, 3 );
+      }
+
+      // Using local data or remote url?
+      if(this.settings.data) {
+        this.request = this.localRequest;
+      }
+      else {
+        this.request = this.remoteRequest;
+      }
+    },
+
+    /**
+     * Register initial mousedown callback
+     * @return {null}
+     */
+    setupEvents: function() {
+      this.el.on('keyup', '.autocomplete-field', $.proxy(this.onKeyUp, this));
+      this.el.on('keydown', '.autocomplete-field', $.proxy(this.onKeyDown, this));
+      this.el.on('click', '.autocomplete-item', $.proxy(this.onClickItem, this));
+
+      // Scroll to field if we're on a small device, we need that
+      // screen real estate!
+      if(this.settings.scrollOnFocus) {
+        this.field.on('focus', function() {
+          var h = $(this).offset().top;
+          setTimeout(function(){
+            window.scrollTo(0, h);
+          }, 0);
+        });
+      }
+    },
+
+    /**
+     * Wrap field in required markup and set up this.el
+     * @return {null}
+     */
+    setupMarkup: function() {
+      this.field.addClass('autocomplete-field');
+      this.field.wrap('<div class="autocomplete" />');
+      this.el = this.field.parent();
+    },
+
+    /**
+     * Final gateway before the search fires: are we allowed to send,
+     * or was the last search too recent? If it was, we delay this
+     * search until we're allowed to do it again.
+     * @param  {string} val Value to search for
+     * @return {null}
+     */
+    limitedRequest: function(val) {
+      if(new Date().getTime() > this.searchTimeLimit) {
+        this.request(val);
+      }
+      else {
+        var that = this;
+        clearTimeout(this.searchTimeout);
+        this.searchTimeout = setTimeout(function() {
+          that.request(val);
+        }, this.searchTimeLimit - new Date().getTime());
+      }
+    },
+
+    /**
+     * Fire request to specified url
+     * @param  {string} val Value to search for
+     * @return {null}
+     */
+    remoteRequest: function(val) {
+      var data = {};
+      data[this.settings.queryProperty] = val;
+      $.ajax({
+        method: this.settings.method,
+        url: this.settings.url,
+        dataType: 'json',
+        data: data,
+        success: $.proxy(this.onReceiveData, this)
+      });
+
+      if(this.settings.timeLimit) {
+        this.searchTimeLimit = new Date().getTime() + this.settings.timeLimit;
+      }
+    },
+
+    /**
+     * Match request in local data instead of remote url
+     * @param  {string} val Value to search for
+     * @return {null}
+     */
+    localRequest: function(val) {
+      var r;
+      if(this.settings.grouped) {
+        this.onReceiveData( this.matchLocalPatternGrouped(val) );
+      }
+      else {
+        this.onReceiveData( this.matchLocalPatternFlat(val) );
+      }
+    },
+
+    /**
+     * Match the data in a flat array and return the objects where a
+     * hit was found.
+     * @param  {string} val Value to search for
+     * @return {array}      Array of hits
+     */
+    matchLocalPatternFlat: function(val) {
+      return this.matchArray( val, this.settings.data );
+    },
+
+    /**
+     * Match the data in a grouped array and return the objects where a
+     * hit was found, in the same manner
+     * @param  {string} val Value to search for
+     * @return {array}      Array of hits
+     */
+    matchLocalPatternGrouped: function(val) {
+      var r = $.extend(true, [], this.settings.data);
+
+      for(var i=0;i<r.length;i++) {
+        var a = this.matchArray(val, r[i].data);
+        if(a.length == 0) {
+          r.splice(i, 1);
+          i--;
+        }
+        else {
+          r[i].data = a;
+        }
+      }
+
+      return r;
+    },
+
+    /**
+     * Return an array of matched values from a source array
+     * @param  {string} val String to search for
+     * @param  {array} arr  Array to search in
+     * @return {array}      Matched array
+     */
+    matchArray: function(val, arr) {
+      var r = [];
+      for(var i=0;i<arr.length;i++) {
+        for(var j in arr[i]) {
+          if( (arr[i][j].toLowerCase && arr[i][j].toLowerCase().indexOf( val.toLowerCase() ) > -1) ||
+              (arr[i][j] == val) ) {
+            r.push(arr[i]);
+            break;
+          }
+        }
+      }
+
+      return r;
+    },
+
+    /**
+     * Find DOM object at specified index
+     * @param  {number} i Index
+     * @return {object}   jQuery object
+     */
+    itemAt: function(i) {
+      if(i == null) {
+        return $();
+      }
+      return $('.autocomplete-item').eq(i);
+    },
+
+    /**
+     * Find item number for clicked item
+     * @param  {object} o The clicked object
+     * @return {number}   Item's id in array
+     */
+    clickedItemAt: function(o) {
+      for(var i=0;i<this.items.length;i++) {
+        if(o == this.el.find('.autocomplete-item').eq(i).get(0)) {
+          return i;
+        }
+      }
+      return null;
+    },
+
+    /**
+     * Select previous item
+     * @return {null}
+     */
+    prevItem: function() {
+      this.selectedItem--;
+      if(this.selectedItem < 0) {
+        this.selectedItem = null;
+      }
+
+      this.markSelected( this.selectedItem );
+    },
+
+    /**
+     * Select next item
+     * @return {null}
+     */
+    nextItem: function() {
+      if(this.selectedItem == null) {
+        this.selectedItem = -1;
+      }
+      this.selectedItem++;
+
+      var l = (this.settings.lastItemTemplate) ? this.items.length : this.items.length-1;
+      if(this.selectedItem >= l) {
+        this.selectedItem = l;
+      }
+
+      this.markSelected( this.selectedItem );
+    },
+
+    /**
+     * Mark an item as selected
+     * @param  {number} i Item's id in items array
+     * @return {null}
+     */
+    markSelected: function(i) {
+      this.el.find('.active').removeClass('active');
+      this.itemAt( i ).addClass('active');
+    },
+
+    /**
+     * Iterate through data, adding <strong> tags for every hit
+     * @param  {object} v   Data to operate on
+     * @param  {string} str String to search for
+     * @return {object}     Processed data object
+     */
+    markHitText: function(v, str) {
+      for(var i in v) {
+        if(typeof(v[i]) == 'string' && i != 'template') {
+          v[i] = v[i].replace( new RegExp("(" + str + ")" , 'gi'), "<strong>$1</strong>" );          
+        }
+      }
+      return v;
+    },
+
+    /**
+     * Render groups. This must be done before calling
+     * renderItemsInGroups, since it resets the list markup and
+     * attaches a new this.list object.
+     * @return {null}
+     */
+    renderGroups: function() {
+      this.list.remove();
+      this.list = $('<ul class="autocomplete-list" />');
+      for(var i in this.json) {
+        this.list.append( this.template( this.settings.groupTemplate, this.json[i] ) );
+      }
+
+      this.el.append( this.list );
+    },
+
+    /**
+     * Iterates through items and attaches them to their respective
+     * groups.
+     * @return {null}
+     */
+    renderItemsInGroups: function() {
+      var v = this.field.val();
+      for(var i=0;i<this.json.length;i++) {
+        var group = this.el.find( this.settings.groupContentName ).eq(i);
+        for(var j=0;j<this.json[i].data.length && j<this.settings.maxItems;j++) {
+          var jsonData = $.extend({}, this.json[i].data[j]);
+          // Strongify hits
+          if(this.settings.markAsBold) {
+            jsonData = this.markHitText( jsonData, v )
+          }
+          group.append( this.template( this.json[i].template || this.settings.itemTemplate, jsonData ) );
+        }
+      }
+    },
+
+    /**
+     * If we're not dealing with groups, just attach items to the list.
+     * @return {null}
+     */
+    renderItemsFlat: function() {
+      this.list.remove();
+      this.list = $('<ul class="autocomplete-list" />');
+      var v = this.field.val();
+      for(var i=0;i<this.json.length && i<this.settings.maxItems;i++) {
+        var jsonData = $.extend({}, this.json[i]);
+        // Strongify hits
+        if(this.settings.markAsBold) {
+          jsonData = this.markHitText( jsonData, v )
+        }
+        this.list.append( this.template( this.json[i].template || this.settings.itemTemplate, jsonData ) );
+      }      
+      this.el.append( this.list );
+    },
+
+    /**
+     * If there's a last "find all results for..." item, this function
+     * gets called.
+     * @return {null}
+     */
+    renderLastItem: function() {
+      this.list.append( this.template( this.settings.lastItemTemplate, {title: this.field.val()} ) );
+    },
+
+    /**
+     * Removes list from DOM and resets state.
+     * @return {null}
+     */
+    closeList: function() {
+      $('.main').off('click');
+      this.list.remove();
+      this.selectedItem = null;
+    },
+
+    /**
+     * Since groups aren't selectable, we need to fetch the actual
+     * selectable items and keep them around in our this.items list.
+     * @return {array} List of selectable items
+     */
+    getItemsFromGroups: function() {
+      var r = [];
+      for(var i in this.json) {
+        for(var j=0;j<this.json[i].data.length;j++) {
+          if(j < this.settings.maxItems) {
+            r.push(this.json[i].data[j]);
+          }
+        }
+      }
+
+      return r;
+    },
+
+    /**
+     * Check whether a value has changed. Used to determine whether or
+     * not we fire a new request, since all keypresses generate an
+     * event.
+     * @return {boolean} True if value has changed since last request
+     */
+    valueHasChanged: function() {
+      if(this.field.val() != this.lastSearch) {
+        this.lastSearch = this.field.val();
+        return true;
+      }
+      return false;
+    },
+
+    /**
+     * A naive implementation of touch detection. In this day and age
+     * with Microsoft Surfaces and whatnot, this should probably be
+     * replaced with something more intelligent. But let's do that when
+     * we find the time.
+     * @return {boolean} Whether or not this is a touch device
+     */
+    isTouchDevice: function() {
+      return !!('ontouchstart' in window);
+    },
+
+    /**
+     * Data received from server, determine what to do with it and
+     * render everything.
+     * @param  {object} data JSON from server
+     * @param  {object} xhr  jQuery XHR object
+     * @return {null}
+     */
+    onReceiveData: function(data, xhr) {
+      this.selectedItem = null;
+      this.json = data;
+      if(this.settings.grouped) {
+        // First, render groups
+        this.renderGroups();
+
+        // Then, render the selectable items
+        this.items = this.getItemsFromGroups();
+        this.renderItemsInGroups();
+      }
+      else {
+        this.items = this.json;
+        this.renderItemsFlat();
+      }
+
+      // Render last item, if applicable
+      if(this.settings.lastItemTemplate) {
+        this.renderLastItem();
+      }
+
+      // Click outside should close the list
+      $('.main').one('click', $.proxy(this.closeList, this));
+    },
+
+    /**
+     * Keyup handler that sets up a delayed check if the field value is
+     * new. If the keyboardDelay option isn't set, it does the check
+     * immediately.
+     * @param  {object} e Normalized jQuery event object
+     * @return {null}
+     */
+    onKeyUp: function(e) {
+      if(this.settings.keyboardDelay) {
+        var that = this;
+        clearTimeout(this.keyboardTimeout);
+        this.keyboardTimeout = setTimeout(function() {
+          that.checkFieldValue();
+        }, this.settings.keyboardDelay);
+      }
+      else {
+        this.checkFieldValue();
+      }
+    },
+
+    /**
+     * Check whether the field value is the same as last time and
+     * satisfies minimum character limit. If yes, the final check is
+     * whether the timeLimit option is set.
+     * @return {null}
+     */
+    checkFieldValue: function() {
+      if(this.field.val().length >= this.settings.minChars && this.valueHasChanged()) {
+        this.limitedRequest( this.field.val() );
+      }
+      if(this.field.val() == '') {
+        this.closeList();
+      }
+    },
+
+    /**
+     * Keydown handler to take care of keyboard navigation.
+     * @param  {object} e Normalized jQuery event object
+     * @return {null}
+     */
+    onKeyDown: function(e) {
+      if(e.keyCode == 38) {  // Up
+        e.preventDefault();
+        this.prevItem();
+      }
+      if(e.keyCode == 40) {  // Down
+        e.preventDefault();
+        this.nextItem();
+      }
+      if(e.keyCode == 13) {  // Enter
+        e.preventDefault();
+        this.onPressEnter();
+      }
+      if(e.keyCode == 27) {  // Esc
+        e.preventDefault();
+        this.closeList();
+      }
+    },
+
+    /**
+     * Click handler, handle mouse/touch selection of items.
+     * @param  {object} e Normalized jQuery event object
+     * @return {null}
+     */
+    onClickItem: function(e) {
+      var i = this.clickedItemAt(e.currentTarget);
+      this.onSelect(e.currentTarget, this.items[i]);
+    },
+
+    /**
+     * User has pressed enter, select object or perform search.
+     * @return {null}
+     */
+    onPressEnter: function() {
+      this.onSelect( this.itemAt( this.selectedItem ), this.items[this.selectedItem] );
+    },
+
+    /**
+     * User has selected an object with mouse or keyboard - act on it
+     * and fire any user-supplied callback.
+     * @param  {object} item jQuery object for the field
+     * @param  {object} val  Item value from server
+     * @return {null}
+     */
+    onSelect: function(item, val) {
+      if(this.settings.onSelect) {
+        this.settings.onSelect.apply(this.field, [item, val]);
+      }
+      this.lastSearch = this.field.val();
+      this.closeList();
+    }
+  };
+
+  TinyAutocomplete.defaults = TinyAutocomplete.prototype.defaults;
+
+  $.fn.tinyAutocomplete = function(settings) {
+    return this.each(function() {
+      if(this.tinyAutocomplete) {
+        // Prevent TinyAutocomplete from creating a new object. Instead,
+        // just update the settings object
+        $.extend(this.tinyAutocomplete.settings, settings);
+        return this;
+      }
+
+      var d = new TinyAutocomplete(this, settings).init();
+      this.tinyAutocomplete = {settings: d.settings};
+    });
+  };
+
+  $.tinyAutocomplete = TinyAutocomplete;
+})(window, $);
